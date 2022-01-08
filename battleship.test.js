dac8e9378c806cdd67a247d61f32d1910bef7826
Jason Bedini <jasonbedini@gmail.com>
2022-01-08 13:19:56 -0800

    finished player factory function

--- battleship.test.js
+++ battleship.test.js
@@ -1,5 +1,6 @@
 import { shipFactory } from "./src/battleship-game-logic.js";
 import { gameboardFactory } from "./src/battleship-game-logic.js";
+import { player } from "./src/battleship-game-logic.js";
 
 const mockShip = shipFactory(2);
 const mockBoard = gameboardFactory(1);
@@ -21,8 +22,30 @@ test('create a gameboard', () => {
     expect(gameboardFactory()).not.toBeUndefined();
 });
 
-// after placeShip, check new values of board, placeShip should return the board
 test('check if ship gets placed', () => {
     const result = mockBoard.placeShip(0, 0, 5, 'chesapeake', 'horizontal');
-    expect(result[1]).toEqual({'0':['not damaged','not damaged', 'not damaged', 'not damaged', 'not damaged', 'nothing here', 'nothing here', 'nothing here']})
+    expect(result[1]).toEqual({'0':['chesapeake, not damaged','chesapeake, not damaged', 'chesapeake, not damaged', 'chesapeake, not damaged', 'chesapeake, not damaged', 'nothing here', 'nothing here', 'nothing here', 'nothing here', 'nothing here']})
+}); 
+
+test("create players, sink all of one player's ships", () => {
+    const human = player('human');
+    const dog = player('dog');
+    human.playerBoard.placeShip(0, 0, 2, 'bay', 'horizontal');
+    human.playerBoard.placeShip(5, 6, 2, 'crimson', 'vertical');
+    dog.makeAttack(0, 0, human.playerBoard);
+    dog.makeAttack(0, 1, human.playerBoard);  
+    dog.makeAttack(5, 6, human.playerBoard);  
+    dog.makeAttack(6, 6, human.playerBoard); 
+    expect(human.playerBoard.checkAllShips()).toBeTruthy() 
+}); 
+
+test("computer player can eventually sink all of player's ships", () => {
+    const computer = player('computer');
+    const human = player('human');
+    human.playerBoard.placeShip(0, 0, 2, 'bay', 'horizontal');
+    human.playerBoard.placeShip(5, 6, 2, 'crimson', 'vertical');
+    while(human.playerBoard.checkAllShips() === false) {
+        computer.makeAttack(0, 0, human.playerBoard);
+    };
+    expect(human.playerBoard.checkAllShips()).toBeTruthy() 
 });
--- src/battleship-game-logic.js
+++ src/battleship-game-logic.js
@@ -10,16 +10,15 @@ const shipFactory = (length, nameOfShip) => {
     // a name that will be used as a key in an object containing multiple ships
     const shipName = nameOfShip;
 
-    /* Passes in the section or sections of the ship that is/are hit
-    and marks the corresponding index(es) as hit in the condition array */
-    const hit = (status, sectionsOfShipTargeted) => {
-        condition[sectionsOfShipTargeted] = status; // status is something like(coordinates, 'hit')
-        return condition;
+    /* Passes in the section of the ship that is hit
+    and marks the corresponding index as hit in the condition array */
+    const hit = (status, sectionsOfShip) => {
+        condition[sectionsOfShip] = status; // status is something like(coordinates, 'hit')
     };
 
     // filter the condition array to see if all indexes contain 'hit', if so the ship is sunk
-    let regex = /.*hit/; // regex expression for matching when determining whether a ship has sunk
     const isSunk = () => {
+        let regex = /.*hit/; // regex expression for matching when determining whether a ship has sunk
         const filteredCondition = condition.filter(status => regex.test(status)); // FIX: USE REGULAR EXPRESSION
         // the number of hits(length of the filtered array) should correspond to the length of the ship if it is sunk
         if(filteredCondition.length === length) {
@@ -40,7 +39,7 @@ const gameboardFactory = (gridSize) => {
     // create an array that represents a row in the gameboard
     const createRow = () => {
         const row = [];
-        for(let i = 0; i < 8; i++) {
+        for(let i = 0; i < 10; i++) {
             row.push('nothing here');
         };
         return row;
@@ -55,7 +54,7 @@ const gameboardFactory = (gridSize) => {
         };
         const board = {};
         for(let i = 0; i < gridSize; i++) {
-            board[i] = createRow(gridSize);
+            board[i] = createRow();
         };
         return board;    
     };
@@ -66,27 +65,26 @@ const gameboardFactory = (gridSize) => {
     const placeShip = (rowNumber, columnNumber, lengthOfShip, nameOfShip, orientation) => {
         // need to make sure row number is a string
         let stringifiedRowNumber = rowNumber.toString();
-        // make sure columnNumber is an int
-        let stringifiedColumnNumber = columnNumber.toString();
         // create the ship
         const ship = shipFactory(lengthOfShip, nameOfShip);
-        // based on orientation of ship modifying board values will be different
+        ships[ship.shipName] = ship;
+        // based on orientation of ship, adding ships to the board will involve one of two algorithms
         if(orientation === 'horizontal') {
             let row = board[`${stringifiedRowNumber}`] // make sure rownumber is a string for property access
             for(let i = 0; i < lengthOfShip; i++) {
-                row[columnNumber] = 'not damaged';
+                row[columnNumber] = `${ship.shipName}, not damaged`;
                 ship.condition.push(`${rowNumber}${columnNumber}, not damaged`)
                 columnNumber += 1; // after each loop move to the next column
             };
-            ships[ship.shipName] = ship;
         }
         else if(orientation === 'vertical'){
             let row;
             for(let i = 0; i < lengthOfShip; i++) {
                 row = board[`${stringifiedRowNumber}`] // get the array present at the row number
-                row[columnNumber] = 'not damaged'; // update the correct index of the array
+                row[columnNumber] = `${ship.shipName}, not damaged`; // update the correct index of the array
+                ship.condition.push(`${rowNumber}${columnNumber}, not damaged`);
                 rowNumber += 1; // after each loop move to the next row
-                newRowNumber = rowNumber.toString();
+                stringifiedRowNumber = rowNumber.toString();
             };
         };
         return [ship, board];
@@ -98,26 +96,30 @@ const gameboardFactory = (gridSize) => {
         if(row[columnNumber] === 'miss' || row[columnNumber] === 'hit') {
             return
         };
-        /* iterate through keys, check if array contains coordinate, if so that's our ship, get index of coordinate,
-         use index of coordinate to modify condition array for the ship, update board 'not damaged' to 'hit'*/
-        /*  if no ship contains those coordinates, then modify the board, replace 'nothing here' with 'miss'*/
+        
         const attackCoordinates = `${rowNumber}${columnNumber}`;
-        for(let key in ships) {
-           let ship = ships[key];
-           if(ship.condition.includes(`${attackCoordinates}, not damaged`)) {
-               // update ship's condition
-               let index = ship.condition.indexOf(`${attackCoordinates}, not damaged`);
-               let status = `${attackCoordinates}, hit`
-               ship.hit(status, index);
-
-               // update the board
-               row[columnNumber] = 'hit';
-               return 'hit'
-           } else {
-               row[columnNumber] = 'miss';
-               return 'miss'
-           }
-        }
+    
+       // see if a ship name is present at the coordinate
+       let regexShip = /.*not\sdamaged/;
+       if(regexShip.test(row[columnNumber])) {
+           const coordinateData = row[columnNumber];
+           let coordinateDataArray = coordinateData.split(',');
+           const shipName = coordinateDataArray[0];
+           coordinateDataArray[1] = ' hit';
+           const newCoordinateData = coordinateDataArray.join(',');
+           row[columnNumber] = newCoordinateData;
+           
+           // update ship's condition
+           let ship = ships[shipName];
+           let index = ship.condition.indexOf(`${attackCoordinates}, not damaged`);
+           let status = `${attackCoordinates}, hit`
+           ship.hit(status, index);
+       }
+           
+       else {
+           row[columnNumber] = 'miss';
+           return;
+       };
     };
 
     const checkAllShips = () => {
@@ -134,6 +136,151 @@ const gameboardFactory = (gridSize) => {
     return { board, placeShip, receiveAttack, checkAllShips };
 };
 
-// ships.ship[hitzones] = arrray
+const player = (name) => {
 
-export { shipFactory, gameboardFactory };
+    // store an array of attacks made, if most recent one is a hit, check adjacent coordinates
+    const attacks = [];
+    const playerName = name;
+    const playerBoard = gameboardFactory(10);
+
+    const makeAttack = (rowNumber, columnNumber, opponentBoard) => {
+        // computer will follow this path
+        if(name === 'computer') {
+            // check if computer's most recent attack is a hit, if so 
+            if(attacks.length > 0) {
+                let regex = /.*hit/;
+                let mostRecentAttack = attacks[attacks.length - 1];
+                if(regex.test(mostRecentAttack)) {
+                    let recentAttackArray = mostRecentAttack.split('');
+                    let newRowNumber = parseInt(recentAttackArray[0]);
+                    let newColumnNumber = parseInt(recentAttackArray[1]);
+                    
+                    // test the strategy of checking left, right, up, and down from the successful attack
+                    const outcome = testStrategy(newRowNumber, newColumnNumber, opponentBoard);
+                    // if the strategy works then we can return, and end the computer's turn
+                    if(outcome === true) {
+                        return;
+                    };
+                };
+            };
+
+            // if most recent attack is not a hit find a valid random attack for the computer
+            let result = false; // make sure it gets tested at least once, set result to false
+            let randomRowNumber;
+            let randomColumnNumber;
+            while(result === false) {
+                randomRowNumber = getRandomInt(0,9);
+                randomColumnNumber = getRandomInt(0,9);
+                // test whether the random numbers are valid
+                result = testAttack(randomRowNumber, randomColumnNumber)
+            };
+            // once a valid attack is obtained, make the attack
+            const attackResult = opponentBoard.receiveAttack(randomRowNumber, randomColumnNumber);
+            attacks.push(`${randomRowNumber}${randomColumnNumber}, ${attackResult}`)
+        }
+        // human controlled path
+        else {
+            opponentBoard.receiveAttack(rowNumber, columnNumber)
+        };
+    };
+
+    // determine whether the computer is set to make a valid attack
+    const testAttack = (rowNumber, columnNumber) => {
+        const gameBoard = playerBoard.board;
+        const row = gameBoard[rowNumber.toString()];
+        const status = row[columnNumber];
+        let regexHit = /.*miss/;
+        let regexMiss = /.*hit/;
+
+        // check whether it has already made an attack on this coordinate or its an invalid coordinate
+        if(regexHit.test(status)|| regexMiss.test(status) || status === undefined || status === null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
+
+    // modify coordinates and check for validity that coordinate has not been previously attacked
+    const testStrategy = (rowNumber, columnNumber, opponentBoard) => {
+        let result;
+        // rowNumber +1 check
+        let rowNumberPlus = rowNumber + 1;
+        result = testAttack(rowNumberPlus, columnNumber);
+        if(result === true) {
+            const attackResult = opponentBoard.receiveAttack(rowNumberPlus, columnNumber);
+            attacks.push(`${rowNumberPlus}${columnNumber}, ${attackResult}`)
+            return true;
+        };
+        // rowNumber - 1 check
+        let rowNumberMinus = rowNumber - 1;
+        result = testAttack(rowNumberMinus, columnNumber);
+        if(result === true) {
+            const attackResult = opponentBoard.receiveAttack(rowNumberMinus, columnNumber);
+            attacks.push(`${rowNumberMinus}${columnNumber}, ${attackResult}`)
+            return true;
+        };
+        // columnNumber + 1 check
+        let columnNumberPlus = columnNumber + 1;
+        result = testAttack(rowNumber, columnNumberPlus);
+        if(result === true) {
+            const attackResult = opponentBoard.receiveAttack(rowNumber, columnNumberPlus);
+            attacks.push(`${rowNumber}${columnNumberPlus}, ${attackResult}`)
+            return true;
+        }
+        // columnNumber - 1 check
+        let columnNumberMinus = columnNumber - 1;
+        result = testAttack(rowNumber, columnNumberMinus);
+        if(result === true) {
+            const attackResult = opponentBoard.receiveAttack(rowNumber, columnNumberMinus);
+            attacks.push(`${rowNumber}${columnNumberMinus}, ${attackResult}`)
+            return true;
+        }
+        return false;
+    };
+
+    const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      }
+    return {playerName, playerBoard, makeAttack}
+};
+
+const DOM = () => {
+    // need initial event listeners added
+
+    // event listener additions that will be called later as needed(eg. placing ships)
+};
+
+//TODO: create a function to randomly place the computer's ships
+
+const gameLoop = (name) => {
+    const humanPlayer = player(name);
+    const computerPlayer = player('computer');
+
+    // gameloop continues as long as either player has not had all of their ships sunken
+    while(humanPlayer.playerBoard.checkAllShips() === false && computerPlayer.playerBoard.checkAllShips() === false) {
+    
+    };
+    const determineWinner = () => {
+        const humanLost = humanPlayer.playerBoard.checkAllShips();
+        const computerLost= computerPlayer.playerBoard.checkAllShips();
+        if(humanLost === true) {
+            return 'Computer Wins';
+        }
+        else if (computerLost === true) {
+            return 'Human Wins';
+        }
+        else {
+            return 'No Winner Yet';
+        }
+    };
+    return { determineWinner }
+};
+
+
+
+
+
+export { shipFactory, gameboardFactory, player, gameLoop };
